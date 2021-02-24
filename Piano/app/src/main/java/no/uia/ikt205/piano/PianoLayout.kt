5b3c8028505a76647eccd2f23696290aff095e78
Vetle Nesland Evensen <vetlene@uia.no>
2021-02-24 18:54:26 +0100

    Added piano assignment 1

--- Piano/app/src/main/java/no/uia/ikt205/piano/PianoLayout.kt
+++ Piano/app/src/main/java/no/uia/ikt205/piano/PianoLayout.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_piano.view.*
+import no.uia.ikt205.piano.data.Note
 import no.uia.ikt205.piano.databinding.FragmentPianoBinding
+import java.io.File
+import java.io.FileOutputStream
 
 class PianoLayout : Fragment() {
 
@@ -13,6 +17,9 @@ class PianoLayout : Fragment() {
     private val binding get() = _binding!!
 
     private val fullTones = listOf("C", "D", "E", "F", "G", "A", "B","C2", "D2", "E2", "F2", "G2")
+    private val semiTones = listOf("C#", "D#", "F#", "G#", "A#", "C#2", "D#2", "F#2", "G#2", "A#2")
+
+    private var score:MutableList<Note> = mutableListOf<Note>() // Score == Noteark?
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,23 +35,57 @@ class PianoLayout : Fragment() {
         val fm = childFragmentManager
         val ft = fm.beginTransaction()
 
-        fullTones.forEach {
-            val fullTonePianoKey = FullTonePianoKeyFragment.newInstance(it)
+        fullTones.forEach { orgNoteValue ->
+            val fullTonePianoKey = FullTonePianoKeyFragment.newInstance(orgNoteValue)
+            var startPlay:Long = 0
 
-            fullTonePianoKey.onKeyDown = {
-                println("Piano key down $it")
+            fullTonePianoKey.onKeyDown = { note ->
+                startPlay = System.nanoTime()
+                println("Piano key down $note")
             }
 
             fullTonePianoKey.onKeyUp = {
+                var endPlay = System.nanoTime()
+                val note = Note(it, startPlay, endPlay)
+                score.add(note)
                 println("Piano key up $it")
             }
 
-            ft.add(view.pianoKeys.id.fullTonePianoKey, "note $it")
+            ft.add(view.pianoKeys.id,fullTonePianoKey, "note $orgNoteValue")
+        }
+
+        semiTones.forEach { orgNoteValue ->
+            val semiTonePianoKey = SemiTonePianoKeyFragment.newInstance(orgNoteValue)
+
+            semiTonePianoKey.onKeyDown = {
+                println("Black piano key down $it")
+            }
+
+            semiTonePianoKey.onKeyUp = {
+                println("Black piano key up $it")
+            }
+
+            ft.add(view.pianoKeys.id,semiTonePianoKey, "note $orgNoteValue")
         }
 
         ft.commit()
 
-        return inflater.inflate(R.layout.fragment_piano, container, false)
+       view.saveScoreBt.setOnClickListener {
+           var fileName = view.fileNameTextEdit.text.toString()
+           val path = this.activity?.getExternalFilesDir(null)
+           if (score.count() > 0 && fileName.isNotEmpty() && path != null){
+               fileName = "$fileName.musikk"
+               FileOutputStream(File(path, fileName),true).bufferedWriter().use { writer ->
+                   //bufferedWriter lever her
+                   score.forEach {
+                       writer.write("${it.toString()}\n")
+                   }
+               }
+           } else {
+               /// TODO: What to do?
+           }
+       }
+        return view
     }
 
 }
--- Piano/app/src/main/java/no/uia/ikt205/piano/SemiTonePianoKeyFragment.kt
+++ Piano/app/src/main/java/no/uia/ikt205/piano/SemiTonePianoKeyFragment.kt
@@ -6,12 +6,12 @@ import android.view.LayoutInflater
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_full_tone_piano_key.view.*
-import no.uia.ikt205.piano.databinding.FragmentFullTonePianoKeyBinding
+import kotlinx.android.synthetic.main.fragment_semi_tone_piano_key.view.*
+import no.uia.ikt205.piano.databinding.FragmentSemiTonePianoKeyBinding
 
-class FullTonePianoKeyFragment : Fragment() {
+class SemiTonePianoKeyFragment : Fragment() {
 
-    private var _binding:FragmentFullTonePianoKeyBinding? = null
+    private var _binding: FragmentSemiTonePianoKeyBinding? = null
     private val binding get() = _binding!!
     private lateinit var note:String
 
@@ -29,16 +29,15 @@ class FullTonePianoKeyFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
-       _binding = FragmentFullTonePianoKeyBinding.inflate(inflater)
+        _binding = FragmentSemiTonePianoKeyBinding.inflate(inflater)
         val view = binding.root
 
-        view.fullToneKey.setOnTouchListener(object : View.OnTouchListener{
+        view.semiToneKey.setOnTouchListener(object : View.OnTouchListener{
             var startTime = 0
             override fun onTouch(v: View?, event: MotionEvent?): Boolean {
                 when(event?.action){
-                    MotionEvent.ACTION_DOWN -> this@FullTonePianoKeyFragment.onKeyDown?.invoke(note)
-                    MotionEvent.ACTION_UP -> this@FullTonePianoKeyFragment.onKeyUp?.invoke(note)
+                    MotionEvent.ACTION_DOWN -> this@SemiTonePianoKeyFragment.onKeyDown?.invoke(note)
+                    MotionEvent.ACTION_UP -> this@SemiTonePianoKeyFragment.onKeyUp?.invoke(note)
                 }
                 return true
             }
@@ -50,7 +49,7 @@ class FullTonePianoKeyFragment : Fragment() {
     companion object {
         @JvmStatic
         fun newInstance(note: String) =
-            FullTonePianoKeyFragment().apply {
+            SemiTonePianoKeyFragment().apply {
                 arguments = Bundle().apply {
                     putString("NOTE", note)
                 }
